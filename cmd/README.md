662f6d6d519bb9335096e118028298951e513efe
Michael Meng <michael.meng@indexexchange.com>
2020-01-09 16:44:46 -0500

    modified cmd README to better reflect current prototypes

--- cmd/README.md
+++ cmd/README.md
@@ -2,11 +2,19 @@
 
 Main applications for this project.
 
-The directory name for each application should match the name of the executable you want to have (e.g., `/cmd/myapp`).
+Currently Holds
+
+1) cmd/test -> Test application that has basic go program 
+
+2) cmd/email -> interface based emailing program, with mock unit tests and how to use dependnacy injection
+
+3) Simple Concurrency program (to do)
+
+<!-- The directory name for each application should match the name of the executable you want to have (e.g., `/cmd/myapp`).
 
 Don't put a lot of code in the application directory. If you think the code can be imported and used in other projects, then it should live in the `/pkg` directory. If the code is not reusable or if you don't want others to reuse it, put that code in the `/internal` directory. You'll be surprised what others will do, so be explicit about your intentions!
 
-It's common to have a small `main` function that imports and invokes the code from the `/internal` and `/pkg` directories and nothing else.
+It's common to have a small `main` function that imports and invokes the code from the `/internal` and `/pkg` directories and nothing else. -->
 
 Examples:
 
--- cmd/test/main.go
+++ cmd/test/main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"os"
 )
 
+//Conventions
+//lower case private
+//Upper case public
+
+//Create error struct
+type myError struct{}
+
+//using the Error function from error interface
+func (myErr *myError) Error() string {
+	return "Error Handler Hit"
+}
+
+type userDetails struct {
+	userName string
+}
+
 //...
-func Greet(writer io.Writer, name string) {
-	fmt.Fprintf(writer, "Greetings, %s", name)
+func Greet(user userDetails) error {
+	user.userName = "Michael \n"
+	if len(user.userName) < 5 {
+		errorDetails(5)
+	}
+	println(user.userName)
+	// fmt.Fprintf(writer, "Greetings, %s", name)
+	return nil
 }
 
 //...
@@ -16,12 +36,26 @@ func Sum(x int, y int) int {
 	return x + y
 }
 
-func main() {
-	Greet(os.Stdout, "Michael \n")
+//Error Handler
+func errorDetails(enum int) {
+	myErr := &myError{} // implements error
+	//default return
+	fmt.Println(myErr)
+}
 
-	sum := Sum(5, 5)
+func main() {
+	var user userDetails
+
+	// user.userName = "A\n"
+
+	// if len(user.userName) < 5 {
+	// 	errorDetails(5)
+	// } else {
+	Greet(user)
+
+	// sum := Sum(5, 5)
 
 	//print sum to console
 
-	fmt.Printf("sum is %d \n", sum)
+	fmt.Printf("End of Run!\n")
 }
